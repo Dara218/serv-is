eba8a1ced5bc1eccb5587fc89adcf0c5824649bb
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-04-29 22:56:52 +0800

    Created RegisterController, route grp register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('components.login');
+});
+
+Route::prefix('register')->name('register.')->group(function(){
+    Route::get('/register', [RegisterController::class, 'create'])->name('create');
 });
