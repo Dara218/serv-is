355efb8db389c42d95b7eb23f8cb7357d8c72325
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-03 16:03:42 +0800

    Added validation on redirection to users, admin

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use RealRashid\SweetAlert\Facades\Alert;
@@ -9,6 +10,7 @@ use RealRashid\SweetAlert\Facades\Alert;
 class SessionController extends Controller
 {
     public function store(Request $request){
+        // return $request;
         $userDetails = $request->validate([
             'username' => 'required',
             'password' => 'required'
@@ -21,7 +23,18 @@ class SessionController extends Controller
         }
 
         session()->regenerate();
-        return redirect()->route('home.index');
+
+        if($request->user_type === 'Customer' && User::where('username', $request->username)->exists()) {
+            return redirect()->route('home.index');
+        }
+
+        if ($request->user_type === 'Client' && User::where('username', $request->username)->exists()) {
+            if(User::where('username', $request->username)->where('user_type', 1)->exists()) {
+                return redirect()->route('home.indexAdmin');
+            }
+            return redirect()->route('home.indexAgent');
+        }
+
     }
 
     public function logout(){
