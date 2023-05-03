4eecb51d81ecd0b0ed6db98dc2dc43a4d09e3233
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-03 16:00:30 +0800

    Added index() and indexAdmin()

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,13 @@ class HomeController extends Controller
     public function index(){
         return view('components.home.home', ['services' => Service::all()]);
     }
+
+    public function indexAgent(){
+        return view('components.home_agent.index');
+    }
+
+    public function indexAdmin(){
+        return view('components.home_admin.index');
+    }
+
 }
