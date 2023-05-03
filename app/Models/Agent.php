0b48fce1e4e901f2e811e83c33c7292b4ff3edb5
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-03 16:07:40 +0800

    Created Agent model

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Agent extends Model
+class Agent extends Authenticatable
 {
-    use HasFactory;
+     use HasFactory, Notifiable;
+
+     protected $fillable = [
+         'name', 'email', 'password',
+     ];
+
+     protected $hidden = [
+         'password', 'remember_token',
+     ];
+
+     protected $casts = [
+         'email_verified_at' => 'datetime',
+     ];
 }
