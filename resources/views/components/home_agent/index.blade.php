1feb0ba03d07f543ab414ff8e4a8f6ffd493398f
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-03 16:13:40 +0800

    Created admin home

---
+++ resources/views/components/home_agent/index.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    @include('partials.navbar')
+
+    {{-- <div class="flex flex-col items-center justify-center min-h-screen my-10"> --}}
+        {{-- <div class="md:w-1/2 w-11/12 flex flex-col justify-center items-center"> --}}
+        <div class="px-16 mt-20">
+            <p>agent</p>
+        </div>
+
+
+
+        {{-- </div> --}}
+    {{-- </div> --}}
+</x-layout>
