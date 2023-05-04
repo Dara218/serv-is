cdde01dc1a5e35312ec920b60642459c5a5d45a8
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-04 22:59:03 +0800

    Created services.blade

---
+++ resources/views/components/home/services.blade.php
@@ -0,0 +1,30 @@
+<div>
+    <div class="text-2xl font-bold text-slate-400 flex justify-between">
+        <span>Services</span>
+        <a href="#" class="text-gray-400 font-medium text-lg">View All</a>
+    </div>
+
+    <div class="flex flex-col md:grid grid-cols-2 gap-4 mt-4">
+
+        <div class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between">
+            <div class="flex flex-col text-left">
+                <span>98.00</span>
+                <span>Client's Wallet</span>
+            </div>
+            <div class="flex flex-col justify-center">
+                image
+            </div>
+        </div>
+
+        <div class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between">
+            <div class="flex flex-col text-left">
+                <span>15</span>
+                <span>Total Transaction</span>
+            </div>
+            <div class="flex flex-col justify-center">
+                image
+            </div>
+        </div>
+
+    </div>
+</div>
