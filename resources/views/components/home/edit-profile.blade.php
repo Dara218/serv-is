0417bda3767e482e9e1bea2dbc2b179453fd3eb3
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-04 22:57:46 +0800

    Created edit-home-profile

---
+++ resources/views/components/home/edit-profile.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    @include('partials.navbar', ['admin' => $admin])
+
+        <div class="px-16 mt-20 flex flex-col gap-8">
+            <span>edit profile</span>
+        </div>
+
+</x-layout>
