047ed8b7c7ff3ec4053da1b2a10db813c471072f
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-04 16:00:18 +0800

    Added $admin on index()

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Service;
+use App\Models\ValidDocument;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('components.home.home', ['services' => Service::all()]);
+
+        $admins = ValidDocument::where('user_type', 1)->get();
+        foreach($admins as $admin){
+
+        }
+
+        return view('components.home.home', ['services' => Service::all(),
+                                            'admin' => $admin]);
     }
 
     public function indexAgent(){
