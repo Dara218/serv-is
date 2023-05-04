fe1279cd30da2d67043f39c5516d68901435ef2a
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-04 22:57:17 +0800

    Added responsive grid cols

--- resources/views/components/home/categories.blade.php
+++ resources/views/components/home/categories.blade.php
@@ -1,9 +1,9 @@
 <div>
     <span class="text-2xl font-bold text-slate-400">Categories</span>
-    <div class="grid grid-cols-3 gap-4 mt-4">
+
+    <div class="grid grid-cols-2 md:grid-cols-3 gap-4 mt-4">
         @foreach ($services as $service)
-            <div class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center">
-                {{-- <div class=""></div> --}}
+            <div class="p-4 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center">
                 <span>{{ ucwords($service->type) }}</span>
             </div>
         @endforeach
