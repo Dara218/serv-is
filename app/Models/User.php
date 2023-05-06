999141296fafcb94ddbb31b88e1129acc796a01a
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-06 21:22:27 +0800

    Added validDocuments()

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function validDocuments(){
+        return $this->hasOne(ValidDocument::class, 'user_id');
+    }
 }
