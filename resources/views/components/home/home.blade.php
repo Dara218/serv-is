f57164e5ace5153f57dbeb88188aeb0f7c7d3742
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-06 21:28:22 +0800

    Removed $admin on navbar

--- resources/views/components/home/home.blade.php
+++ resources/views/components/home/home.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    @include('partials.navbar', ['admin' => $admin])
+    @include('partials.navbar')
 
     {{-- <div class="flex flex-col items-center justify-center min-h-screen my-10"> --}}
         {{-- <div class="md:w-1/2 w-11/12 flex flex-col justify-center items-center"> --}}
