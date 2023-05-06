8a27f4cb58d7d693f9c699191cc44aba9b221fe0
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-06 21:24:42 +0800

    Added $admin on navbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\ValidDocument;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +23,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
+
+        View::composer('partials.navbar', function($view){
+            $admins = ValidDocument::where('user_type', 1)->get();
+
+            foreach($admins as $admin){
+                $view->with('admin', $admin);
+            }
+
+        });
     }
 }
