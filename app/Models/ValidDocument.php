529bd397ebb086c1d0fb0a45fdab8c59dea7a1e4
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-06 21:22:48 +0800

    Added users()

--- app/Models/ValidDocument.php
+++ app/Models/ValidDocument.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class ValidDocument extends Model
 {
     use HasFactory;
+
+    public function users(){
+        return $this->hasOne(User::class, 'id');
+    }
 }
