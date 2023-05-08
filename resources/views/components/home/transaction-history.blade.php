093ebac8709df1a6bd7712fb837a07d4b3b76aa6
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-08 14:19:29 +0800

    Created transaction blade file

---
+++ resources/views/components/home/transaction-history.blade.php
@@ -0,0 +1,60 @@
+<x-layout>
+    @include('partials.navbar')
+
+        <div class="px-16 my-24">
+            {{ Breadcrumbs::render('transaction-history') }}
+
+            <div class="mt-12 flex flex-col items-center gap-8">
+                <div class="w-full flex flex-col gap-4">
+
+                    {{-- Foreach here --}}
+
+                        <div class="relative overflow-x-auto">
+                            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3">
+                                            Service
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Amount Paid
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Date of transaction
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @if ($transactions->count() == 0)
+
+                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                            <td class="px-6 py-4">
+                                                <span>No recent transaction.</span>
+                                            </td>
+                                        </tr>
+
+                                        @else
+                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                            <td class="px-6 py-4">
+                                                Silver
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                Laptop
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                Laptop
+                                            </td>
+                                        </tr>
+                                    @endif
+
+                                </tbody>
+                            </table>
+                        </div>
+
+                </div>
+            </div>
+        </div>
+
+        @include('sweetalert::alert')
+
+</x-layout>
