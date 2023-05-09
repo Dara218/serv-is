1b245de83ed280b153ff65294b09585a19ca3b3e
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-09 22:45:54 +0800

    Removed photo_id

--- database/migrations/2023_05_02_034907_create_valid_documents_table.php
+++ database/migrations/2023_05_02_034907_create_valid_documents_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('valid_documents', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
-            $table->string('photo_id');
+            // $table->string('photo_id');
             $table->string('nbi_clearance');
             $table->string('police_clearance');
             $table->string('birth_certificate');
