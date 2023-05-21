a39f2cde9c50d55846ac40b3146e605f5e597e90
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-21 22:22:25 +0800

    Added chatRoomId, changed to presence channel, Added broadcastWith()

--- app/Events/Message.php
+++ app/Events/Message.php
@@ -7,25 +7,28 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class Message implements ShouldBroadcast
+class Message implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public $username;
     public $message;
-    public $receiver_hidden;
+    public $receiverId;
+    public $chatRoomId;
 
     /**
      * Create a new event instance.
      */
-    public function __construct($username, $message, $receiver_hidden)
+    public function __construct($username, $message, $receiverId, $chatRoomId)
     {
         $this->username = $username;
         $this->message = $message;
-        $this->receiver_hidden = $receiver_hidden;
+        $this->receiverId = $receiverId;
+        $this->chatRoomId = $chatRoomId;
     }
 
     /**
@@ -36,12 +39,20 @@ class Message implements ShouldBroadcast
     public function broadcastOn(): array
     {
         return [
-            // new PrivateChannel('chat'),
-            new Channel('chat'),
+            new PresenceChannel('chat.'.$this->chatRoomId),
+            // new Channel('chat.'.$this->chatRoomId),
         ];
     }
 
     public function broadcastAs(){
         return 'message';
+    }
+
+    public function broadcastWith(){
+        return [
+            'username' => $this->username,
+            'message' => $this->message,
+            'chatRoom' => $this->chatRoomId
+        ];
     }
 }
