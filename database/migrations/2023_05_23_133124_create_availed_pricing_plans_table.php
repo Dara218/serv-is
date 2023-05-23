857baf489a9d5255e3c603bd584a73480fa76f6a
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-23 23:23:36 +0800

    Created availed pricing plans migration

---
+++ database/migrations/2023_05_23_133124_create_availed_pricing_plans_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('availed_pricing_plans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('availed_to_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('availed_by_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('pricing_plan_type')->constrained('pricing_plans')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('availed_pricing_plans');
+    }
+};
