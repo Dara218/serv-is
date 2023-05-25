6241dff8b20c47c1a563d04cbc49d4bb7017a96d
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-25 23:26:00 +0800

    Changed data type of type from string to integer

--- database/migrations/2023_05_23_080253_create_pricing_plans_table.php
+++ database/migrations/2023_05_23_080253_create_pricing_plans_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('pricing_plans', function (Blueprint $table) {
             $table->id();
-            $table->string('type');
+            $table->integer('type');
             $table->integer('price');
             $table->timestamps();
         });
