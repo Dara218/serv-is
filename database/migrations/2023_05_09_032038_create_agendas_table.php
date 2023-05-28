70b6451dff3b4a2be1d47fa21ad5ec6227ce5d5e
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-28 21:51:03 +0800

    Added budget column

--- database/migrations/2023_05_09_032038_create_agendas_table.php
+++ database/migrations/2023_05_09_032038_create_agendas_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->string('message');
             $table->string('service');
+            $table->integer('budget');
             $table->string('deadline');
             $table->timestamps();
         });
