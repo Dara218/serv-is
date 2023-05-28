79ee3c9dcc5a3f343cb916bc91f7723849780559
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-28 21:50:38 +0800

    Changed user() from hasOne to belongsTo

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -10,6 +10,6 @@ class Transaction extends Model
     use HasFactory;
 
     public function user(){
-        return $this->hasOne(User::class, 'user_id');
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
