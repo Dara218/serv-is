04dbc4a21279db3771a85f169edee0c5787eda85
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-28 12:33:15 +0800

    Added "" to all data attributes, Added data on edit and delete btn

--- resources/views/components/home/service-address.blade.php
+++ resources/views/components/home/service-address.blade.php
@@ -13,7 +13,7 @@
                     <div class="w-full mb-6 bg-slate-100 rounded-md py-6 px-4">
 
                         <div class="flex items-center justify-between mb-2">
-                            <span class="font-semibold text-lg primary-address" data-id={{ $primaryAddress->id }}>{{ $primaryAddress->address }}</span>
+                            <span class="font-semibold text-lg primary-address" data-id="{{ $primaryAddress->id }}">{{ $primaryAddress->address }}</span>
                             <label class="relative inline-flex items-center mr-5 cursor-pointer">
 
                                 <form class="form-primary-address-checkbox">
@@ -29,12 +29,13 @@
                         <div>
                             <span>
                                 <a href="#" class="mr-2 hover:text-slate-500 edit-primary-address">Edit</a>
-                                <a href="#" class="hover:text-slate-500 delete-primary-address">Delete</a>
+                                <a href="#" class="hover:text-slate-500 delete-address" data-type="primary" data-id="{{ $primaryAddress->id }}">Delete</a>
                             </span>
                         </div>
                     </div>
 
                     @foreach ($secondaryAddresses as $secondaryAddress)
+
                         <div class="w-full mb-6 bg-slate-100 rounded-md py-6 px-4">
                             <div class="flex items-center justify-between mb-2">
                                 <span class="font-semibold text-lg secondary-address">{{ $secondaryAddress->address }}</span>
@@ -44,15 +45,19 @@
                                         @csrf
                                     </form>
 
-                                    <input type="checkbox" value="" class="sr-only peer checkbox-secondary" data-id={{ $secondaryAddress->id }}>
+                                    <input type="checkbox" value="" class="sr-only peer checkbox-secondary" data-id="{{ $secondaryAddress->id }}">
                                     <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-slate-700 peer-focus:ring-4 peer-focus:ring-teal-300 dark:peer-focus:ring-teal-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-slate-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-teal-600"></div>
                                 </label>
 
                             </div>
                             <div>
                                 <span>
-                                    <a href="#" class="mr-2 hover:text-slate-500 edit-secondary-address">Edit</a>
-                                    <a href="#" class="hover:text-slate-500 delete-secondary-address">Delete</a>
+                                    <a href="#" class="mr-2 hover:text-slate-500 edit-secondary-address" data-id="{{ $secondaryAddress->id }}" data-address="{{ $secondaryAddress->address }}">Edit</a>
+
+                                    <a href="#" class="hover:text-slate-500 delete-address"
+                                    data-type="secondary"
+                                    data-id="{{ $secondaryAddress->id }}">
+                                    Delete</a>
                                 </span>
                             </div>
                         </div>
