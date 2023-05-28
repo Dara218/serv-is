b1b802cb9f0b47ef163fb7646a9ed5f0674ef3d2
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-28 21:42:32 +0800

    Created file, added storeAgenda() (working)

---
+++ app/Http/Controllers/AgendaController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AgendaRequest;
+use App\Models\Agenda;
+use App\Models\Service;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class AgendaController extends Controller
+{
+    public function storeAgenda(AgendaRequest $request){
+        $agendaDetails = $request->validated();
+
+        // todo: show agenda on bottom, agent and admin cant see, user cant
+
+        Agenda::create([
+            'user_id' => Auth::user()->id,
+            'message' => $agendaDetails['message'],
+            'service' => $agendaDetails['service'],
+            'budget' => $agendaDetails['budget'],
+            'deadline' => $agendaDetails['deadline']
+        ]);
+
+        Alert::success('Success', 'Agenda successfully posted');
+        return redirect()->route('home.index');
+
+    }
+
+}
