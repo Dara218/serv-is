1fdca6b13d094ed73eb02d3d8e91a01726243728
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-30 21:54:50 +0800

    Added user() and userPhoto()

--- app/Models/Agenda.php
+++ app/Models/Agenda.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Agenda extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function userPhoto(){
+        return $this->belongsTo(UserPhoto::class, 'user_id');
+    }
 }
