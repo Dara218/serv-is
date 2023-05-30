58c9a2a1a44e1a0f74ce154b33be5242cf59a4f8
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-30 21:55:11 +0800

    Added agenda()

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -64,5 +65,9 @@ class User extends Authenticatable
 
     public function chat(){
         return $this->hasMany(Chat::class, 'id');
+    }
+
+    public function agenda(){
+        return $this->hasMany(Agenda::class, 'id');
     }
 }
