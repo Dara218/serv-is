37917ccf8044fb1c41b02df3c887e50f07bff8f5
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-31 21:58:20 +0800

    Added balance and transaction variable, Linked home-service

--- resources/views/components/home/home.blade.php
+++ resources/views/components/home/home.blade.php
@@ -3,12 +3,16 @@
 
     {{-- <div class="flex flex-col items-center justify-center min-h-screen my-10"> --}}
         {{-- <div class="md:w-1/2 w-11/12 flex flex-col justify-center items-center"> --}}
-        <div class="px-16 mt-20 flex flex-col gap-8">
-            <x-home.categories :services="$services"/>
+        <div class="md:px-16 px-4 mt-16 md:mt-24 flex flex-col gap-8">
+            <x-home.categories/>
 
-            <x-home.wallet-transaction/>
+            <x-home.wallet-transaction :balance="$balance" :transaction="$transaction"/>
 
-            <x-home.services/>
+            <div class="flex justify-center mt-12">
+                <x-home.services-search/>
+            </div>
+
+            <x-home.services :services="$services"/>
         </div>
 
         {{-- </div> --}}
