ce69b3f64bb3a04c309ce8d85e1588b8e021d5c7
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-05-31 21:58:59 +0800

    Made dynamic, added $balance

--- resources/views/components/home/wallet-transaction.blade.php
+++ resources/views/components/home/wallet-transaction.blade.php
@@ -1,25 +1,29 @@
 <div>
     <div class="flex flex-col md:grid grid-cols-2 gap-4 mt-4">
 
-        <div class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between">
-            <div class="flex flex-col text-left">
-                <span>98.00</span>
-                <span>Client's Wallet</span>
+        <a href="{{ route('home.showWallet') }}" class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600">
+            <div class="flex flex-col text-left ">
+                <span class="font-bold text-2xl text-slate-600">P {{ $balance }}</span>
+                <span class="font-semibold text-xltext-slate-500">Client's Wallet</span>
             </div>
             <div class="flex flex-col justify-center">
-                image
+                <span class="material-symbols-outlined">
+                    account_balance_wallet
+                </span>
             </div>
-        </div>
+        </a>
 
-        <div class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between">
+        <a href="{{ route('home.showTransactionHistory') }}" class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600"">
             <div class="flex flex-col text-left">
-                <span>15</span>
-                <span>Total Transaction</span>
+                <span class="font-bold text-2xl text-slate-600">{{ $transaction }}</span>
+                <span class="font-semibold text-xltext-slate-500">Total Transaction</span>
             </div>
             <div class="flex flex-col justify-center">
-                image
+                <span class="material-symbols-outlined">
+                    receipt_long
+                </span>
             </div>
-        </div>
+        </a>
 
     </div>
 </div>
