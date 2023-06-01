694266b0d5630af8d4b05acd35a353f82cd1a256
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-01 21:28:05 +0800

    Added validation for auth users, Added hidden input for auth user id

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,13 +1,20 @@
 
 <nav class="bg-slate-300 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto md:p-2 p4">
-        <a href="{{ route('home.index') }}" class="flex items-center">
+        <a href="{{
+            Auth::user()->user_type == 3 ? route('home.index') :
+            (Auth::user()->user_type == 2 ? route('home.indexAgent') :
+            (Auth::user()->user_type == 1 ? route('home.indexAdmin') : ''))}}"
+            class="flex items-center">
+
             <img src="{{ asset('images/servis_logo.png') }}" class="h-8 mr-3" alt="Flowbite Logo">
             <span class="hidden md:block self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Serv &#9679; is</span>
         </a>
         <div class="flex md:order-2">
             <div>
                 @include('partials.notification')
+
+                <input type="hidden" id="current-user-id" value="{{ Auth::user()->id }}">
 
                 <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                     <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
@@ -45,7 +52,7 @@
                     </form>
                 </div>
             </div>
-            
+
         </div>
     </div>
 </nav>
