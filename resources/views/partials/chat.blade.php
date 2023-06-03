a5050349f8f1c2f33784616c5bf5e12ab880ee68
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-03 21:26:45 +0800

    Added agent-list id for dynamic chat head width

--- resources/views/partials/chat.blade.php
+++ resources/views/partials/chat.blade.php
@@ -1,34 +1,37 @@
 @if (Auth::check())
 <div class="chat-modal md:grid grid-cols-3 bg-slate-100 w-11/12 md:w-1/2 h-96 fixed bottom-12 right-4 rounded shadow flex flex-col gap-2" style="display:none;">
-    <div class="flex flex-col gap-1 col-span-1 bg-slate-200 p-3 shadow-md">
+    <div class="flex flex-col gap-1 col-span-1 bg-slate-200 p-3 shadow-md w-full overflow-hidden h-32 md:h-full">
         <span class="font-semibold">Chats</span>
-        <ol class="flex md:flex-col gap-2">
+        <div class="overflow-x-auto overflow-y-hidden md:overflow-x-hidden md:overflow-y-scroll">
+            <ol class="flex md:flex-col gap-2 md:h-80" id="agent-list">
 
-            @foreach ($agents as $agent)
-                <li class="flex gap-1 receiver-el cursor-pointer shadow-md" data-id="{{ $agent->user->id }}">
-                    <img
-                        src="https://images.pexels.com/photos/7841717/pexels-photo-7841717.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                        alt="user id photo"
-                        class="h-8 w-8 mb-2 receiver-chat-head-click"
-                        style="border-radius: 50%"
-                        data-id="{{ $agent->user->id }}"
-                        data-username="{{ $agent->user->username }}"
-                    >
+                @foreach ($agents as $agent)
+                    <li class="flex gap-1 receiver-el cursor-pointer shadow-md" data-id="{{ $agent->user->id }}">
+                        <img
+                            src="https://images.pexels.com/photos/7841717/pexels-photo-7841717.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                            alt="user id photo"
+                            class="h-8 w-8 mb-2 receiver-chat-head-click"
+                            style="border-radius: 50%"
+                            data-id="{{ $agent->user->id }}"
+                            data-username="{{ $agent->user->username }}"
+                        >
 
-                    <span class="md:block hidden receiver-chat-heads">{{ Auth::user()->user_type == 3 ? $agent->user->username :  (Auth::user()->user_type == 2 ? $agent->availedBy->username : '') }}</span>
-                </li>
-            @endforeach
+                        <span class="md:block hidden receiver-chat-heads">{{ Auth::user()->user_type == 3 ? $agent->user->username :  (Auth::user()->user_type == 2 ? $agent->availedBy->username : '') }}</span>
+                    </li>
+                @endforeach
 
-            <form class="form-chat-head">
-                @csrf
-                <input type="hidden" name="user_id" class="user-id-hidden">
-                <input type="hidden" name="username" id="username-hidden" value="{{ Auth::user()->id }}">
-                <input type="hidden" name="chatId" class="chat-id">
-                <input type="hidden" name="receiver_hidden" id="receiver-chat-head">
-                <button type="submit" class="hidden">submit</button>
-            </form>
+                <form class="form-chat-head">
+                    @csrf
+                    <input type="hidden" name="user_id" class="user-id-hidden">
+                    <input type="hidden" name="username" id="username-hidden" value="{{ Auth::user()->id }}">
+                    <input type="hidden" name="chatId" class="chat-id">
+                    <input type="hidden" name="receiver_hidden" id="receiver-chat-head">
+                    <button type="submit" class="hidden">submit</button>
+                </form>
 
-        </ol>
+            </ol>
+        </div>
+
     </div>
 
     <div class="chat-container col-span-2 pb-12 overflow-y-auto overflow-x-hidden relative h-5/6 md:h-full">
