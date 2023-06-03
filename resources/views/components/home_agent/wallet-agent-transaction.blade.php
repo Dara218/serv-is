e0b4dcb5cc99a2540a75e9362689990f061642fa
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-03 21:26:17 +0800

    Added dynamic data for services, Added element for services, account wallet. and todays service

--- resources/views/components/home_agent/wallet-agent-transaction.blade.php
+++ resources/views/components/home_agent/wallet-agent-transaction.blade.php
@@ -1,29 +1,49 @@
-<div>
-    <div class="flex flex-col md:grid grid-cols-2 gap-4 mt-4">
+<div class="grid grid-cols-2 gap-4">
+    <a href="{{ route('home.showWallet') }}" class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600">
+        <div class="flex flex-col text-left ">
+            <span class="font-bold text-2xl text-slate-600">P {{ $balance }}</span>
+            <span class="font-semibold text-xltext-slate-500">Employee's Wallet</span>
+        </div>
+        <div class="flex flex-col justify-center">
+            <span class="material-symbols-outlined">
+                account_balance_wallet
+            </span>
+        </div>
+    </a>
 
-        <a href="{{ route('home.showWallet') }}" class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600">
-            <div class="flex flex-col text-left ">
-                <span class="font-bold text-2xl text-slate-600">P {{ $balance }}</span>
-                <span class="font-semibold text-xltext-slate-500">Client's Wallet</span>
-            </div>
-            <div class="flex flex-col justify-center">
-                <span class="material-symbols-outlined">
-                    account_balance_wallet
-                </span>
-            </div>
-        </a>
+    <a href="{{ route('home.showTransactionHistory') }}" class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600"">
+        <div class="flex flex-col text-left">
+            <span class="font-bold text-2xl text-slate-600">{{ $services }}</span>
+            <span class="font-semibold text-xltext-slate-500">Total service</span>
+        </div>
+        <div class="flex flex-col justify-center">
+            <span class="material-symbols-outlined">
+                receipt_long
+            </span>
+        </div>
+    </a>
 
-        <a href="{{ route('home.showTransactionHistory') }}" class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600"">
-            <div class="flex flex-col text-left">
-                <span class="font-bold text-2xl text-slate-600">{{ $transaction }}</span>
-                <span class="font-semibold text-xltext-slate-500">Total Transaction</span>
-            </div>
-            <div class="flex flex-col justify-center">
-                <span class="material-symbols-outlined">
-                    receipt_long
-                </span>
-            </div>
-        </a>
+    <a href="#" class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600">
+        <div class="flex flex-col text-left ">
+            <span class="font-bold text-2xl text-slate-600">15</span>
+            <span class="font-semibold text-xltext-slate-500">Upcoming service</span>
+        </div>
+        <div class="flex flex-col justify-center">
+            <span class="material-symbols-outlined">
+                account_balance_wallet
+            </span>
+        </div>
+    </a>
 
-    </div>
+    <a href="#" class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600">
+        <div class="flex flex-col text-left">
+            <span class="font-bold text-2xl text-slate-600">5</span>
+            <span class="font-semibold text-xltext-slate-500">Today's service</span>
+        </div>
+        <div class="flex flex-col justify-center">
+            <span class="material-symbols-outlined">
+                receipt_long
+            </span>
+        </div>
+    </a>
 </div>
