f9295e0e2f1cbe6b8b76452b366e1c16cc427c9d
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-04 21:20:04 +0800

    Added data attr on task-me-agenda btn

--- resources/views/components/home_agent/agenda-home.blade.php
+++ resources/views/components/home_agent/agenda-home.blade.php
@@ -1,5 +1,5 @@
 <span class="text-xl font-semibold mb-1">Agendas</span>
-<div class="grid grid-cols-3 gap-3">
+<div class="grid grid-cols-2 md:grid-cols-3 gap-3">
     @foreach ($agendas as $agenda)
         <div class="grid-cols-span-1">
             <div class="bg-slate-100 rounded-lg p-4 relative">
@@ -33,16 +33,23 @@
                     </div>
                 </div>
 
+                {{-- @dd($request) --}}
+
                 <div class="flex justify-evenly mt-6">
                     <button type="button" class="bg-slate-600 text-white rounded cursor-pointer py-2 px-4 hover:bg-slate-800 btn-negotiate-agenda"
                     data-id="{{ $agenda->id }}"
                     data-message="{{ $agenda->message }}"
                     data-service="{{ $agenda->service }}"
                     data-budget="{{ $agenda->budget }}"
-                    data-username="{{ $agenda->user->username }}" 
+                    data-username="{{ $agenda->user->username }}"
                     data-userid="{{ $agenda->user->id }}">Make Offer</button>
 
-                    <button type="button" class="bg-white rounded cursor-pointer py-2 px-4 hover:bg-gray-200 btn-task-me-agenda" data-id="{{ $agenda->id }}">Task Me</button>
+                    <button type="button" class="bg-white rounded cursor-pointer py-2 px-4 hover:bg-gray-200 btn-task-me-agenda" data-id="{{ $agenda->id }}"
+                    data-message="{{ $agenda->message }}"
+                    data-service="{{ $agenda->service }}"
+                    data-budget="{{ $agenda->budget }}"
+                    data-username="{{ $agenda->user->username }}"
+                    data-userid="{{ $agenda->user->id }}">Task Me</button>
                 </div>
             </div>
         </div>
