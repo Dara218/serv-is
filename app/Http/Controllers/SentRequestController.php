a0291434374382b1ce3274d88382e52e8e0b2c2a
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-04 21:13:45 +0800

    Created file, commented getSentRequest function

---
+++ app/Http/Controllers/SentRequestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SentRequestController extends Controller
+{
+    // public function getSentRequest(Request $request){
+    //     SentRequest::where($request->)
+    // }
+}
