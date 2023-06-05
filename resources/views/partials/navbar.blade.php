70d9f853bb2a77e33aa720d30d4cdebf6e9d14d2
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-05 21:36:51 +0800

    Added banner for authenticated unverified agent

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,6 @@
 
+<x-home_agent.banner :agentservice="$agentservice"/>
+
 <nav class="bg-slate-300 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto md:p-2 p4">
         <a href="{{
@@ -14,37 +16,52 @@
             <div>
                 @include('partials.notification')
 
-                <input type="hidden" id="current-user-id" value="{{ Auth::user()->id }}">
+                <input type="hidden" id="current-user-id" value="{{ Auth::user()->id }}" data-user-service="{{ $user->agentService->id }}">
 
                 <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                     <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
                         <li>
-                        <a href="{{ route('home.showEditProfile') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit Profile</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('home.showWallet') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">My Wallet</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('home.showServiceProvider') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Service Provider</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('home.showServiceAddress') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Service Address</a>
-                    </li>
-                    <li>
-                        <a href="{{route('home.showRewards')}}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Rewards & Discounts</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('home.showTransactionHistory') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Transaction History</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('home.showFaqs') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Customer Support</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('home.showAgenda') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Agenda</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('home.showChat') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Chat</a>
-                    </li>
+                            <a href="{{ route('home.showEditProfile') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit Profile</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('home.showWallet') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">My Wallet</a>
+                        </li>
+                        @if (Auth::user()->user_type == 3)
+                            <li>
+                                <a href="{{ route('home.showServiceProvider') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Service Provider</a>
+                            </li>
+
+                            <li>
+                                <a href="{{ route('home.showServiceAddress') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Service Address</a>
+                            </li>
+                            <li>
+                                <a href="{{route('home.showRewards')}}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Rewards & Discounts</a>
+                            </li>
+                        @endif
+                        <li>
+                            <a href="{{ route('home.showTransactionHistory') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Transaction History</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('home.showFaqs') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Customer Support</a>
+                        </li>
+                        @if (Auth::user()->user_type == 3)
+                            <li>
+                                <a href="{{ route('home.showAgenda') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Agenda</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('home.showChat') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Chat</a>
+                            </li>
+                        @endif
+                        @if (Auth::user()->user_type == 2 && $agentservice)
+                            <li>
+                                <span class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer btn-add-service-title">Add Service title</span>
+                            </li>
+
+                            @elseif (Auth::user()->user_type == 2 && ! $agentservice)
+                                <li>
+                                    <a href="{{ route('home.createServiceDetails') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer btn-update-service">Update Service Details</a>
+                                </li>
+                        @endif
                     </ul>
                     <form class="py-1 w-full" action="{{ route('session.logout') }}" method="post">
                         @csrf
