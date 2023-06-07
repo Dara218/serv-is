7da00a8c695dbbd38ca1df96bac7673476a084d9
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-07 21:50:34 +0800

    Removed comments

--- resources/views/components/home_admin/index.blade.php
+++ resources/views/components/home_admin/index.blade.php
@@ -1,14 +1,6 @@
 <x-layout>
     @include('partials.navbar')
-
-    {{-- <div class="flex flex-col items-center justify-center min-h-screen my-10"> --}}
-        {{-- <div class="md:w-1/2 w-11/12 flex flex-col justify-center items-center"> --}}
         <div class="px-16 mt-20">
             <p>admin</p>
         </div>
-
-
-
-        {{-- </div> --}}
-    {{-- </div> --}}
 </x-layout>
