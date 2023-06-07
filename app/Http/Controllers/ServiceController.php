23080cda6662c953973308f66e5b5355c6e26b80
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-07 21:45:47 +0800

    Added getAgentService and getAllAgentService()

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AgentService;
 use App\Models\Service;
 use Illuminate\Http\Request;
 
@@ -9,8 +10,25 @@ class ServiceController extends Controller
 {
     public function getServices(){
         $services = Service::all();
-
         return response()->json($services);
+    }
 
+    public function getAgentService(){
+        $agentServices = AgentService::where('is_pending', 0)
+                                    ->with('user.userPhoto', 'review', 'service')
+                                    ->inRandomOrder()
+                                    ->take(6)
+                                    ->get();
+
+        return response()->json($agentServices);
+    }
+
+    public function getAllAgentService(){
+        $allAgentServices = AgentService::where('is_pending', 0)
+                                    ->with('user.userPhoto', 'review', 'service')
+                                    ->inRandomOrder()
+                                    ->get();
+
+        return response()->json($allAgentServices);
     }
 }
