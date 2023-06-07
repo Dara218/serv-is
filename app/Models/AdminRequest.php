32ed505581a59d9c94416f12868ae90aa4c65486
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-07 21:46:24 +0800

    Added validDocument()

--- app/Models/AdminRequest.php
+++ app/Models/AdminRequest.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class AdminRequest extends Model
 {
     use HasFactory;
+
+    public function validDocument(){
+        return $this->hasOne(ValidDocument::class, 'user_id', 'request_by');
+    }
+
+    // public function notification(){
+    //     return $this->hasOne(Notification::class, 'from_user_id');
+    // }
 }
