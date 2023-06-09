a4bee32aba2a7732fffaa315095b5e1e32c81c0c
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-09 22:06:42 +0800

    Added parent element to accept and reject button

--- resources/views/components/home_admin/confirm-agent.blade.php
+++ resources/views/components/home_admin/confirm-agent.blade.php
@@ -9,9 +9,15 @@
                         <span class="text-slate-500 font-semibold">{{ $otherDocument->validDocument->notification->username }} joined Serv-is! Accept verification status?
                         </span>
                         <div class="flex gap-4 justify-center py-10 items-center">
-                            <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 bnt-accept-notif" data-id="{{ $otherDocument->validDocument->notification->id }}" data-username="{{ $otherDocument->validDocument->notification->username }}" data-message="{{ $otherDocument->validDocument->notification->message }}" data-from-user-id="{{ $otherDocument->validDocument->notification->from_user_id }}" data-to-user-id="{{ $otherDocument->validDocument->notification->user_id }}" data-type="{{ $otherDocument->validDocument->notification->type }}">Confirm</button>
 
-                            <button type="button" class="tbnt-reject-notif ext-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" data-id="{{ $otherDocument->validDocument->notification->id }}" data-username="{{ $otherDocument->validDocument->notification->username }}" data-message="{{ $otherDocument->validDocument->notification->message }}" data-from-user-id="{{ $otherDocument->validDocument->notification->from_user_id }}" data-to-user-id="{{ $otherDocument->validDocument->notification->user_id }}" data-type="{{ $otherDocument->validDocument->notification->type }}">Reject</button>
+                            <button type="button" class="accepted-rejected-btn hidden ext-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 cursor-default">Accepted</button>
+
+                            <div class="flex gap-4 justify-center items-center confirm-reject-parent">
+                                <button type="button" class="bnt-accept-notif accept-agent focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" data-id="{{ $otherDocument->validDocument->notification->id }}" data-username="{{ $otherDocument->validDocument->notification->username }}" data-message="{{ $otherDocument->validDocument->notification->message }}" data-from-user-id="{{ $otherDocument->validDocument->notification->from_user_id }}" data-to-user-id="{{ $otherDocument->validDocument->notification->user_id }}" data-type="{{ $otherDocument->validDocument->notification->type }}">Confirm</button>
+
+                                <button type="button" class="btn-reject-notif reject-agent ext-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" data-id="{{ $otherDocument->validDocument->notification->id }}" data-username="{{ $otherDocument->validDocument->notification->username }}" data-message="{{ $otherDocument->validDocument->notification->message }}" data-from-user-id="{{ $otherDocument->validDocument->notification->from_user_id }}" data-to-user-id="{{ $otherDocument->validDocument->notification->user_id }}" data-type="{{ $otherDocument->validDocument->notification->type }}">Reject</button>
+                            </div>
+
                         </div>
                         <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
                             <div class="mb-6 grid-cols-span-1 flex flex-col gap-2 justify-center items-center border-t-2 border-slate-400">
@@ -37,6 +43,8 @@
                         </div>
                     </div>
                 @endforeach
+
+                {{ $otherDocuments->links('pagination::tailwind') }}
             </div>
         </div>
 
