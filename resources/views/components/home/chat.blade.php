36288fb3d4648e2ec2bbefe8ef57e26a239247d1
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-09 22:03:36 +0800

    Added validation to show only accepted agents

--- resources/views/components/home/chat.blade.php
+++ resources/views/components/home/chat.blade.php
@@ -14,39 +14,33 @@
 
                         {{-- @dd($agents) --}}
                         @foreach ($agents as $agent)
-                            <div class="mt-4 relative">
+                            @if ($agent->adminRequest->is_accepted == true)
+                                <div class="mt-4 relative">
+                                    <span class="dots material-symbols-outlined absolute top-2 right-2 cursor-pointer ">
+                                        more_horiz
+                                    </span>
 
-                                <span class="dots material-symbols-outlined absolute top-2 right-2 cursor-pointer ">
-                                    more_horiz
-                                </span>
+                                    <a href="{{ route('home.showEmployeeProfile', ['user' => $agent->username]) }}" class="view-profile hidden bg-white rounded w-1/2 h-auto p-2 absolute top-8 right-2 cursor-pointer shadow-md hover:bg-gray-50">
+                                        <span>View Profile</span>
+                                    </a>
 
-                                <a href="{{ route('home.showEmployeeProfile', ['user' => $agent->username]) }}" class="view-profile hidden bg-white rounded w-1/2 h-auto p-2 absolute top-8 right-2 cursor-pointer shadow-md hover:bg-gray-50">
-                                    <span>View Profile</span>
-                                </a>
-
-                                <div class="bg-slate-100 p-6 rounded-md">
-                                    <div class="flex gap-2">
-                                        <img src="{{ $agent->userPhoto->profile_picture }}" alt="user id photo" class="h-1/2 w-16 rounded-full">
-                                        <span class="font-semibold">{{ ucwords($agent->fullname) }}</span>
+                                    <div class="bg-slate-100 p-6 rounded-md">
+                                        <div class="flex gap-2">
+                                            <img src="{{ $agent->userPhoto->profile_picture }}" alt="user id photo" class="h-1/2 w-16 rounded-full">
+                                            <span class="font-semibold">{{ ucwords($agent->fullname) }}</span>
+                                        </div>
+                                        <form action="{{ route('home.storeChat', ['user' => $agent->id]) }}" method="post" class="grid grid-cols-2 gap-2 mt-3">
+                                            @csrf
+                                            <button type="submit" class="col-span-1 bg-slate-600 text-white rounded cursor-pointer py-2 px-4 hover:bg-slate-800 shadow-md">Book Provider</button>
+                                            <button type="button" class="col-span-1 bg-white rounded cursor-pointer py-2 px-4 hover:bg-gray-200 shadow-md">Close</button>
+                                        </form>
                                     </div>
-                                    <form action="{{ route('home.storeChat', ['user' => $agent->id]) }}" method="post" class="grid grid-cols-2 gap-2 mt-3">
-                                        @csrf
-                                        <button type="submit" class="col-span-1 bg-slate-600 text-white rounded cursor-pointer py-2 px-4 hover:bg-slate-800 shadow-md">Book Provider</button>
-                                        <button type="button" class="col-span-1 bg-white rounded cursor-pointer py-2 px-4 hover:bg-gray-200 shadow-md">Close</button>
-                                    </form>
                                 </div>
-                            </div>
+                            @endif
                         @endforeach
-
                     </div>
                 </div>
-
             </div>
-
         </div>
     </div>
-
-
-
-
 </x-layout>
