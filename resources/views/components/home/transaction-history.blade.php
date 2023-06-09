b1730f6e51991a34068674cfa35e1bbe5ed69b7f
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-09 22:06:14 +0800

    Added pagination

--- resources/views/components/home/transaction-history.blade.php
+++ resources/views/components/home/transaction-history.blade.php
@@ -10,7 +10,7 @@
                     {{-- Foreach here --}}
 
                         <div class="relative overflow-x-auto">
-                            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 mb-6">
                                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                     <tr>
                                         <th scope="col" class="px-6 py-3">
@@ -46,11 +46,10 @@
                                             </tr>
                                         @endforeach
                                     @endif
-
                                 </tbody>
                             </table>
+                            {{ $transactions->links('pagination::tailwind') }}
                         </div>
-
                 </div>
             </div>
         </div>
