30d9a1caa36ad8418c36a9fe67a50acce6b55cb8
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-11 21:46:54 +0800

    -Adjusted margin for sm devices, -Changed variables to camcase

--- resources/views/components/home/my-wallet.blade.php
+++ resources/views/components/home/my-wallet.blade.php
@@ -1,14 +1,16 @@
 <x-layout>
     @include('partials.navbar')
 
-        <div class="px-16 my-24">
+        <div class="px-8 md:px-16 my-24">
             {{ Breadcrumbs::render('my-wallet') }}
 
             <div class="mt-12 flex flex-col items-center gap-8">
                 <div class="w-full md:w-1/2 flex flex-col gap-4">
                     <div class="flex gap-2 flex-col">
                         <span class="font-semibold">Current Balance</span>
-                        @include('partials.cash-in', ['checkinprices' => $checkinprices])
+
+                        @include('partials.cash-in', ['checkInPrices' => $checkInPrices])
+
                         <div class="p-4 rounded flex justify-between items-center border border-slate-300">
                             <div class="flex flex-col">
                                 <span class="font-semibold">P{{ $userBalance }}.00</span>
@@ -25,16 +27,17 @@
                     <span class="-mb-2">Wallet History</span>
                     <span class="w-full border-b-2 border-slate-700"></span>
 
-                    {{-- Foreach here --}}
-                    <div class="p-4 rounded flex justify-between items-center border border-slate-300">
-                        <div class="flex flex-col">
-                            <span class="font-semibold">Cash-In</span>
-                            <small>02 Dec, 2022</small>
+                    @foreach ($walletHistories as $walletHistory)
+                        <div class="p-4 rounded flex justify-between items-center border border-slate-300">
+                            <div class="flex flex-col">
+                                <span class="font-semibold">Cash-In</span>
+                                <small>{{ $walletHistory->created_at->format('d M, Y') }}</small>
+                            </div>
+                            <div>
+                                + {{ $walletHistory->amount }}
+                            </div>
                         </div>
-                        <div>
-                            + P98.00
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
