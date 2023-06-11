06d586edff552960f5b28550a62090f2229af109
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-11 21:49:04 +0800

    -Changed button text to Load wallet, -Added full width on modal buttons -Made checkinprices camel case

--- resources/views/partials/cash-in.blade.php
+++ resources/views/partials/cash-in.blade.php
@@ -1,7 +1,7 @@
 
-<button type="button" data-modal-target="crypto-modal" data-modal-toggle="crypto-modal" class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700">
+<button type="button" data-modal-target="crypto-modal" data-modal-toggle="crypto-modal" class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 my-4">
     <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path></svg>
-    Connect wallet
+    Load Wallet
   </button>
 
   <!-- Main modal -->
@@ -21,23 +21,18 @@
               </div>
               <!-- Modal body -->
               <div class="p-6">
-                  <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Connect with one of our available wallet providers or create a new one.</p>
+                  <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Select a number of points you want to cash in.</p>
                   <ul class="my-4 space-y-3">
-                      @foreach ($checkinprices as $checkinprice)
-                        <form action="{{ route('storeCheckIn') }}" method="post" class="check-in-price cursor-pointer" data-price="{{ $checkinprice->prices }}">
+                      @foreach ($checkInPrices as $checkInPrice)
+                        <form action="{{ route('storeCheckIn') }}" method="post" class="check-in-price cursor-pointer" data-price="{{ $checkInPrice->prices }}">
                             {{-- @csrf --}}
-                            <button type="submit" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
-                                P {{ $checkinprice->prices }}
-                                <input type="hidden" name="price" value="{{ $checkinprice->prices }}">
+                            <button type="submit" class="w-full flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+                                P {{ $checkInPrice->prices }}
+                                <input type="hidden" name="price" value="{{ $checkInPrice->prices }}">
                             </button>
                         </form>
                       @endforeach
                   </ul>
-                  <div>
-                      <a href="#" class="inline-flex items-center text-xs font-normal text-gray-500 hover:underline dark:text-gray-400">
-                          <svg aria-hidden="true" class="w-3 h-3 mr-2" aria-hidden="true" focusable="false" data-prefix="far" data-icon="question-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 448c-110.532 0-200-89.431-200-200 0-110.495 89.472-200 200-200 110.491 0 200 89.471 200 200 0 110.53-89.431 200-200 200zm107.244-255.2c0 67.052-72.421 68.084-72.421 92.863V300c0 6.627-5.373 12-12 12h-45.647c-6.627 0-12-5.373-12-12v-8.659c0-35.745 27.1-50.034 47.579-61.516 17.561-9.845 28.324-16.541 28.324-29.579 0-17.246-21.999-28.693-39.784-28.693-23.189 0-33.894 10.977-48.942 29.969-4.057 5.12-11.46 6.071-16.666 2.124l-27.824-21.098c-5.107-3.872-6.251-11.066-2.644-16.363C184.846 131.491 214.94 112 261.794 112c49.071 0 101.45 38.304 101.45 88.8zM298 368c0 23.159-18.841 42-42 42s-42-18.841-42-42 18.841-42 42-42 42 18.841 42 42z"></path></svg>
-                          Why do I need to connect with my wallet?</a>
-                  </div>
               </div>
           </div>
       </div>
