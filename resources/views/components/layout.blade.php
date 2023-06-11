61d31dbf2345dd7cc0b5dff248ba11c7017fa3d1
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-11 21:40:23 +0800

    Unlinked serv_is.js,

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -28,6 +28,7 @@
                 <span class="material-symbols-outlined">
                     chat
                 </span>
+                {{-- <span class="bg-red-400 rounded-full p-1 text-xs text-white">new</span> --}}
             </div>
         @endif
 
@@ -40,7 +41,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js" integrity="sha512-uMtXmF28A2Ab/JJO2t/vYhlaa/3ahUOgj1Zf27M5rOo8/+fcTUVH0/E0ll68njmjrLqOBjXM3V9NiPFL5ywWPQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <script src="{{ asset('js/servis.js') }}"></script>
+        {{-- <script src="{{ asset('js/servis.js') }}"></script> --}}
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
 
     </body>
