baa1a6e87c13bea509f741e1a7db3d67ed7fafa1
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-12 11:34:19 +0800

    Added has_unread boolean column for message badge

--- database/migrations/2023_05_20_071523_create_chats_table.php
+++ database/migrations/2023_05_20_071523_create_chats_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('sender_id')->constrained('users')->cascadeOnDelete();
             $table->foreignId('receiver_id')->constrained('users')->cascadeOnDelete();
+            $table->boolean('has_unread')->default(false);
             $table->timestamps();
         });
     }
