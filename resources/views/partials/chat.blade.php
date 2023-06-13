e149355cb6805c1b66e0cf6d3e0edce4d035c12b
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-13 21:15:47 +0800

    -Refactor chat loop to handle different user types -Added padding

--- resources/views/partials/chat.blade.php
+++ resources/views/partials/chat.blade.php
@@ -1,11 +1,11 @@
 @if (Auth::check())
-    <div class="chat-modal md:grid grid-cols-3 bg-slate-100 w-11/12 md:w-1/2 h-96 fixed bottom-12 right-4 rounded shadow flex flex-col gap-2" style="display:none;">
+    <div class="chat-modal md:grid grid-cols-3 z-10 bg-slate-100 w-11/12 md:w-1/2 h-96 fixed bottom-12 right-4 rounded shadow flex flex-col gap-2" style="display:none;">
         <div class="flex flex-col gap-1 col-span-1 bg-slate-200 p-3 shadow-md w-full overflow-hidden h-32 md:h-full">
             <span class="font-semibold">Chats</span>
             <div class="overflow-x-auto overflow-y-hidden md:overflow-x-hidden md:overflow-y-scroll">
                 <ol class="flex md:flex-col gap-2 md:h-80" id="agent-list">
                     @foreach ($agents as $agent)
-                        @foreach ($agent->user->chat as $chat)
+                        @foreach ( Auth::user()->user_type == 1 ? $agent->chat :   $agent->user->chat as $chat)
                             <li class="flex items-center gap-1 p-1 receiver-el cursor-pointer shadow-md" data-chat-id="{{ $chat->id }}" data-sender="{{ $chat->sender_id }}" data-receiver="{{ $chat->receiver_id }}" {{ Auth::user()->user_type == 1 ? 'data-id=' .$agent->id  : 'data-id=' .$agent->user->id }} data-username="{{ Auth::user()->user_type == 1 ? $agent->username : (Auth::user()->user_type == 3 ? $agent->user->username : (Auth::user()->user_type == 2 ? $agent->availedBy->username : '')) }}">
                                 <img
                                     src="https://images.pexels.com/photos/7841717/pexels-photo-7841717.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -45,7 +45,7 @@
             </div>
         </div>
 
-        <div class="chat-container col-span-2 pb-12 overflow-y-auto overflow-x-hidden relative h-5/6 md:h-full">
+        <div class="chat-container col-span-2 pb-12 overflow-y-auto overflow-x-hidden relative h-5/6 md:h-full p-2">
             <span class="font-semibold mb-2 sticky top-0 bg-slate-100 w-full shadow-md flex justify-center items-center current-chat-name py-2" style="display: none;">Name of current chat</span>
             <span class="font-semibold mb-2 sticky top-0 w-full flex justify-center items-center gap-1 cursor-pointer load-more" style="display: none;">
                 <span>Load More</span>
