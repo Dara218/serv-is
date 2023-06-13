0c9589a9f7a5178b87752a8fced24d9af4229bb4
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-13 21:13:12 +0800

    Moved routes getCategories() and getAgentService()

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/get-categories', [CategoryController::class, 'getCategories'])->name('getCategories');
+Route::get('/get-agent-service', [ServiceController::class, 'getAgentService'])->name('getAgentService');
+
