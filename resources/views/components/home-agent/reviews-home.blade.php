8c4a516727cfbbf5024444765ad05f62d00ac498
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-15 22:00:31 +0800

    Created review home

---
+++ resources/views/components/home-agent/reviews-home.blade.php
@@ -0,0 +1,10 @@
+@if ($reviews->count() == 0)
+    <span class="font-semibold text-slate-400 flex justify-center items-center">No reviews.</span>
+
+    @else
+        <div class="user-review-el flex flex-col gap-6">
+            @foreach ($reviews as $review)
+                <x-home.reviews :review="$review"/>
+            @endforeach
+        </div>
+@endif
