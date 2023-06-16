be4c058dda61d83bdb69fca098727202895b4afa
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-16 21:52:24 +0800

    Added margin top

--- resources/views/components/home-agent/reviews-home.blade.php
+++ resources/views/components/home-agent/reviews-home.blade.php
@@ -2,7 +2,7 @@
     <span class="font-semibold text-slate-400 flex justify-center items-center">No reviews.</span>
 
     @else
-        <div class="user-review-el flex flex-col gap-6">
+        <div class="user-review-el flex flex-col gap-6 mt-6">
             @foreach ($reviews as $review)
                 <x-home.reviews :review="$review"/>
             @endforeach
