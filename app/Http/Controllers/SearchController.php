49ca82d393e14d2356de6f184cc369676cc34b84
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-17 22:02:00 +0800

    Refactored getUsers function -Improved user search functionality

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\AgentService;
 use App\Models\Service;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 
@@ -20,7 +21,7 @@ class SearchController extends Controller
                                                     ->get();
         }
         else
-        { 
+        {
             $searchAgentServiceResults = AgentService::where('title', 'LIKE', '%'. $request->searchValue .'%')
                                                     ->where('service_id', $serviceType->id)
                                                     ->with('service', 'user.userPhoto', 'review')
@@ -47,5 +48,30 @@ class SearchController extends Controller
 
         $searchResults = ['agentService' => $searchServiceResults];
         return response()->json($searchResults);
+    }
+
+    public function getUsers(Request $request)
+    {
+        $userType = $request->userType;
+        $searchValue = $request->searchValue;
+        $role = 1;
+
+        if($userType === "Agent"){
+            $role = 2;
+        }
+        else if($userType === "Customer"){
+            $role = 3;
+        }
+
+        $users = User::where('fullname', 'LIKE' , '%'. $searchValue .'%')
+                    ->orWhere('username', 'LIKE' , '%'. $searchValue .'%')
+                    ->where('user_type', '!=', $role)
+                    ->get();
+
+        if($searchValue == null){
+            $users = User::where('user_type', '!=', 1)->get();
+        }
+
+        return response()->json(['users' => $users]);
     }
 }
