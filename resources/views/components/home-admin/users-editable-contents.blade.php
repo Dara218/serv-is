21ea2e0263808233aa0b57b57abae131f2ee5098
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-17 22:07:36 +0800

    Editable contents on admin view front end

---
+++ resources/views/components/home-admin/users-editable-contents.blade.php
@@ -0,0 +1,25 @@
+<div class="grid grid-cols-2 gap-4">
+    <a href="{{ route('showTransactionHistory') }}" class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600"">
+        <div class="flex flex-col text-left">
+            <span class="font-bold text-2xl text-slate-600">All Users</span>
+            <span class="font-semibold text-xltext-slate-500">Manage Users</span>
+        </div>
+        <div class="flex flex-col justify-center">
+            <span class="material-symbols-outlined">
+                receipt_long
+            </span>
+        </div>
+    </a>
+
+    <a href="#" class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600">
+        <div class="flex flex-col text-left ">
+            <span class="font-bold text-2xl text-slate-600">Manage Serv-is</span>
+            <span class="font-semibold text-xltext-slate-500">Update serv-is contents</span>
+        </div>
+        <div class="flex flex-col justify-center">
+            <span class="material-symbols-outlined">
+                account_balance_wallet
+            </span>
+        </div>
+    </a>
+</div>
