9d4091914b7adc99c3d7f8936d07a21ba82df47d
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-17 22:07:01 +0800

    Users table on admin view frontend

---
+++ resources/views/components/home-admin/users-table.blade.php
@@ -0,0 +1,28 @@
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Full Name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Username
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Contacy No.
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Status
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    <span class="sr-only">Edit</span>
+                </th>
+            </tr>
+        </thead>
+
+        <tbody class="user-table-body">
+
+            {{-- {{ $users->links() }} --}}
+        </tbody>
+    </table>
+</div>
