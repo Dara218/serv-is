45fd80f8a1a68dcecca7128c06d3651dedbf0be9
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-17 22:04:16 +0800

    New middleware for multi user access

---
+++ app/Http/Middleware/UserAccess.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use RealRashid\SweetAlert\Facades\Alert;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $role): Response
+    {
+        if(Auth::check() && Auth::user()->user_type == $role)
+        {
+            return $next($request);
+        }
+
+        Auth::logout();
+        Alert::error('Failed', $role);
+        return redirect('/');
+    }
+}
