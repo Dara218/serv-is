25aaeb985c4f441c22c8d2fc76a2f3637ae82ba9
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-17 22:11:01 +0800

    Added routes for getUsers for user search on admin

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\ServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,4 +25,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/get-categories', [CategoryController::class, 'getCategories'])->name('getCategories');
 Route::get('/get-agent-service', [ServiceController::class, 'getAgentService'])->name('getAgentService');
-
+Route::get('/get-users', [SearchController::class, 'getUsers'])->name('getUsers');
