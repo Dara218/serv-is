3bdc07732944f8121885a1492aabe09d26cedf20
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-17 22:05:00 +0800

    Added role attribute accessor to the User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -42,6 +44,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected function role(): Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => ["customer", "agent", "admin"][$value],
+        );
+    }
 
     public function validDocuments(){
         return $this->hasOne(ValidDocument::class, 'user_id');
