06d621ef393a382667b23f57b28327e0aa4d40ed
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-18 21:46:46 +0800

    Added user relationship to the model

--- app/Models/ContactUs.php
+++ app/Models/ContactUs.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class ContactUs extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class, 'id');
+    }
 }
