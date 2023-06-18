eef76c032f10d28ec9790c3233444942c4d6ba44
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-18 21:59:02 +0800

    Updated table and user management UI.

--- resources/views/components/home-admin/users-table.blade.php
+++ resources/views/components/home-admin/users-table.blade.php
@@ -1,5 +1,5 @@
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <table class="w-full text-sm text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" class="px-6 py-3">
@@ -20,9 +20,17 @@
             </tr>
         </thead>
 
-        <tbody class="user-table-body">
+        <tbody class="user-table-body"></tbody>
 
-            {{-- {{ $users->links() }} --}}
-        </tbody>
+        @foreach ($users as $user)
+            <div data-simplebar data-simplebar-auto-hide="false" id="manage-admin-btn-{{ $user->username }}" class="-translate-x-1/2 -translate-y-1/2 fixed h-[calc(100%-1rem)] hidden left-1/2 manage-admin-btn-dara218 max-h-full overflow-x-hidden overflow-y-auto top-1/2 transform w-full z-50">
+                @include('components.home-admin.edit-user-modal', ['users' => $users])
+            </div>
+        @endforeach
     </table>
+    
+    <p class="no-user-search w-full font-semibold isolate-nightmode my-5 text-center hidden">No user found.</p>
+    <div class="isolate-nightmode my-5 text-center ">
+        @include('partials.spinner')
+    </div>
 </div>
