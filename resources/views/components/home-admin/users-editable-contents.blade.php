fb7d41828ae8206e02deb3ac16c7f7f14d9fff07
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-18 21:57:45 +0800

    Updated link and text -for managing user concerns in the customer support section. Updated the padding and styling of the link container in code snippet

--- resources/views/components/home-admin/users-editable-contents.blade.php
+++ resources/views/components/home-admin/users-editable-contents.blade.php
@@ -1,8 +1,8 @@
 <div class="grid grid-cols-2 gap-4">
-    <a href="{{ route('showTransactionHistory') }}" class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600"">
-        <div class="flex flex-col text-left">
-            <span class="font-bold text-2xl text-slate-600">All Users</span>
-            <span class="font-semibold text-xltext-slate-500">Manage Users</span>
+    <a href="{{ route('showCustomerConcern') }}" class="p-3 md:p-5 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600">
+        <div class="flex flex-col justify-center text-left">
+            <span class="font-bold text-2xl text-slate-600">Customer Support</span>
+            <span class="font-semibold text-xltext-slate-500">Manage user concerns</span>
         </div>
         <div class="flex flex-col justify-center">
             <span class="material-symbols-outlined">
@@ -11,8 +11,8 @@
         </div>
     </a>
 
-    <a href="#" class="p-10 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600">
-        <div class="flex flex-col text-left ">
+    <a href="#" class="p-3 md:p-5 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600">
+        <div class="flex flex-col justify-center text-left">
             <span class="font-bold text-2xl text-slate-600">Manage Serv-is</span>
             <span class="font-semibold text-xltext-slate-500">Update serv-is contents</span>
         </div>
