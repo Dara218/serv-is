0ca02a4705405e6f1d454e4b373028550e975e27
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-19 22:02:34 +0800

    Added routes on updateConcernStatus

--- routes/web.php
+++ routes/web.php
@@ -109,4 +109,5 @@ Route::middleware(['auth', 'user-access:1'])->group(function()
     Route::get('/show-confirm-agent/{user}', [AgentServiceController::class, 'showConfirmAgent'])->name('showConfirmAgent');
     Route::get('/show-confirm-agent-admin', [AgentServiceController::class, 'showConfirmAgentOnNav'])->name('showConfirmAgentOnNav');
     Route::get('/manage-customer-concern', [ContactUsController::class, 'index'])->name('showCustomerConcern');
+    Route::put('/update-concern-status/{id}', [ContactUsController::class, 'update'])->name('updateConcernStatus');
 });
