c675522b687b1656effdb8971fa85c82b2cb423f
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-20 21:39:04 +0800

    Added routes for getServicesAdmin and getRewards

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\MessageController;
+use App\Http\Controllers\PricingPlanController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RewardController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\ServiceController;
 use Illuminate\Http\Request;
@@ -28,3 +30,6 @@ Route::get('/get-agent-service', [ServiceController::class, 'getAgentService'])-
 Route::get('/get-users', [SearchController::class, 'getUsers'])->name('getUsers');
 Route::get('/get-user-from-dropdown', [SearchController::class, 'getUsersFromDropdown'])->name('getUsersFromDropdown');
 Route::get('/get-customer-concerns', [SearchController::class, 'getCustomerConcerns'])->name('getCustomerConcerns');
+Route::get('/get-services-admin', [ServiceController::class, 'getServices'])->name('getServicesAdmin');
+Route::get('/get-pricing-plan', [PricingPlanController::class, 'getPricingPlan'])->name('getPricingPlan');
+Route::get('/get-rewards', [RewardController::class, 'getRewards'])->name('getRewards');
