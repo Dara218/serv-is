08cef696b1711bbff58fbaf30501575049b2ba7b
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-20 21:35:26 +0800

    Fix website link in HTML code

--- resources/views/components/home-admin/users-editable-contents.blade.php
+++ resources/views/components/home-admin/users-editable-contents.blade.php
@@ -11,7 +11,7 @@
         </div>
     </a>
 
-    <a href="#" class="p-3 md:p-5 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600">
+    <a href="{{ route('showManageWebsite') }}" class="p-3 md:p-5 h-auto grid-cols-span-1 border border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600">
         <div class="flex flex-col justify-center text-left">
             <span class="font-bold text-2xl text-slate-600">Manage Serv-is</span>
             <span class="font-semibold text-xltext-slate-500">Update serv-is contents</span>
