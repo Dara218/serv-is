c2638339bcf062f40e0a7829864c720ce73f46f5
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-21 21:49:12 +0800

    Update reward based on the provided request data.

--- app/Http/Controllers/RewardController.php
+++ app/Http/Controllers/RewardController.php
@@ -10,4 +10,15 @@ class RewardController extends Controller
     public function getRewards(){
         return response()->json(Reward::all());
     }
+
+    public function updateReward($id, Request $request)
+    {
+        Reward::find($id)->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'points' => $request->points
+        ]);
+
+        return response()->json($request);
+    }
 }
