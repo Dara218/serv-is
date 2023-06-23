f2a784d1b71746e067db042679eb172614185f06
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-23 22:07:44 +0800

    Changed id name to dynamic Changed data-model value Changed text of transaction date to date Refactor service section in view file

--- resources/views/components/home-admin/user-2nd-modal.blade.php
+++ resources/views/components/home-admin/user-2nd-modal.blade.php
@@ -1,15 +1,15 @@
-@props(['type', 'number_type', 'userType', 'user'])
+@props(['type', 'numberType', 'userType', 'user'])
 
-<div id="{{ $type }}-modal" tabindex="-1" class="z-10 fixed top-0 left-0 right-0 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div id="{{ $type }}-${{ $user->username }}-modal" tabindex="-1" class="z-50 fixed top-0 left-0 right-0 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative w-full max-w-md max-h-full">
         <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div class="relative bg-white rounded-lg shadow-2xl dark:bg-gray-700">
             <!-- Modal header -->
             <div class="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
                 <h3 class="text-xl font-medium text-gray-900 dark:text-white">
                     {{ ucwords($type) }}
                 </h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="{{$type}}-modal">
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="{{ $type }}-${{ $user->username }}-modal">
                     <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     <span class="sr-only">Close modal</span>
                 </button>
@@ -34,7 +34,7 @@
                             </div>
                         </div>
                     @endforeach
-            
+
                 @elseif ($type === 'review')
                     @foreach ($user->review as $review)
                         <div class="grid grid-cols-3 gap-4 mb-4 border border-b p-4">
@@ -47,12 +47,11 @@
                                 <div>{{ $review->message }}</div>
                             </div>
                             <div>
-                                <div class="grid-cols-span-1 text-slate-500 font-semibold">Transaction Date</div>
+                                <div class="grid-cols-span-1 text-slate-500 font-semibold">Date</div>
                                 <div>{{ $review->created_at->format('d M, Y') }}</div>
                             </div>
                         </div>
                     @endforeach
-            
 
                 @elseif ($type === 'agenda')
                     @foreach ($user->agenda as $agenda)
@@ -71,9 +70,25 @@
                             </div>
                         </div>
                     @endforeach
+
+                @elseif ($type === 'service')
+                    <div class="grid grid-cols-3 gap-4 mb-4 border border-b p-4">
+                        <div>
+                            <div class="grid-cols-span-1 text-slate-500 font-semibold">Service Title</div>
+                            <div>{{ $user->agentService->title }}</div>
+                        </div>
+                        <div>
+                            <div class="grid-cols-span-1 text-slate-500 font-semibold">Pending</div>
+                            <div>{{ $user->agentService->is_pending ?  'Yes' : 'No' }}</div>
+                        </div>
+                        <div>
+                            <div class="grid-cols-span-1 text-slate-500 font-semibold">Has Pending Changes</div>
+                            <div>{{ $user->agentService->is_pending_changes ? 'Yes' : 'No' }}</div>
+                        </div>
+                    </div>
                 @endif
-                
             </div>
         </div>
     </div>
 </div>
+
