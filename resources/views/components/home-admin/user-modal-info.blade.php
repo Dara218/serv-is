4164c68d8f61b50d7445182d1fc3b41c5c5285b4
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-23 22:08:19 +0800

    Added variables for dynamic process

--- resources/views/components/home-admin/user-modal-info.blade.php
+++ resources/views/components/home-admin/user-modal-info.blade.php
@@ -1,9 +1,9 @@
-@props(['type', 'number_type', 'userType', 'user'])
+@props(['type', 'numberType', 'userType', 'user'])
 
-<div data-modal-target="{{ $type }}-modal" data-modal-toggle="{{ $type }}-modal" class="p-4 h-auto grid-cols-span-1 border cursor-pointer border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600">
+<div data-modal-target="{{ $type }}-${{ $user->username }}-modal" data-modal-toggle="{{ $type }}-${{ $user->username }}-modal" class="p-4 h-auto grid-cols-span-1 border cursor-pointer border-slate-300 rounded-xl text-center flex justify-between hover:border-slate-600">
     <div class="flex flex-col text-left ">
         <span class="font-bold text-2xl text-slate-600">{{ ucwords($type) }}</span>
-        <span class="font-semibold text-xltext-slate-500">No. of {{ $type }}: {{ $number_type }}</span>
+        <span class="font-semibold text-xltext-slate-500">No. of {{ $type }}: {{ $numberType }}</span>
     </div>
     <div class="flex flex-col justify-center">
         @if ($type == 'review')
@@ -22,4 +22,4 @@
     </div>
 </div>
 
-<x-home-admin.user-2nd-modal :user="$user" type="{{ $type }}" number_type="{{ $number_type }}" userType="{{ $userType }}"/>
+<x-home-admin.user-2nd-modal :user="$user" type="{{ $type }}" numberType="{{ $numberType }}" userType="{{ $userType }}"/>
