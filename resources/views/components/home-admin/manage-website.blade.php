ff957000dbe43472f723dde76ca120c74a8da478
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-23 22:00:55 +0800

    -Added margin on classes

--- resources/views/components/home-admin/manage-website.blade.php
+++ resources/views/components/home-admin/manage-website.blade.php
@@ -5,7 +5,7 @@
 
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div class="services-table-parent">
-                    <div class="see-all-service-btn-parent"></div>
+                    <div class="see-all-service-btn-parent mb-2"></div>
                     <x-home-admin.tables.contents-table type="service" column1="Type" column2="Action"/>
                 </div>
 
@@ -15,13 +15,13 @@
                 </div> --}}
 
                 <div class="pricing-plan-table-parent">
-                    <div class="see-all-pricing-plan-btn-parent"></div>
+                    <div class="see-all-pricing-plan-btn-parent mb-2"></div>
                     <x-home-admin.tables.contents-table type="pricing-plan" column1="Type" column2="Action"/>
                 </div>
             </div>
 
             <div class="rewards-table-parent">
-                <div class="see-all-rewards-btn-parent"></div>
+                <div class="see-all-rewards-btn-parent mb-2"></div>
                 <x-home-admin.tables.contents-table type="rewards" column1="Type" column2="Action"/>
             </div>
         </div>
