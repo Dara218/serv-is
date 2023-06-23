5497a501d71ce1957328d84e2367eaead03194c4
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-23 22:09:31 +0800

    Removed logo link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -37,7 +37,7 @@
             </ul>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{ date('Y') }} <a href="https://flowbite.com/" class="hover:underline">Serv &#9679; is™</a>. All Rights Reserved.</span>
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{ date('Y') }} <span class="hover:underline">Serv &#9679; is™</span>. All Rights Reserved.</span>
     </div>
 </footer>
 
