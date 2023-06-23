98af42da84d38181c51f60890bdc6961c12707f9
Ralph Bautista <ralph.breis.bautista@gmail.com>
2023-06-23 21:53:48 +0800

    Updated date formatting for agenda deadline

--- resources/views/components/home/agenda.blade.php
+++ resources/views/components/home/agenda.blade.php
@@ -48,7 +48,9 @@
                                                 </span>
                                                 <span class="flex items-center gap-2">
                                                     <span>Deadline: </span>
-                                                    {{ $agenda->deadline }}
+                                                    <span class="{{ $agenda->deadline < now() ? 'text-red-500' : 'text-green-500' }}">
+                                                        {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $agenda->deadline)->format('F j, Y, g:i A') }}
+                                                    </span>
                                                 </span>
                                             </div>
                                         </div>
@@ -118,18 +120,11 @@
                                     <div class="mb-6">
                                         <label for="deadline" class="block mb-2 text-sm font-medium text-slate-500 dark:text-white">Deadline</label>
 
-                                        {{-- <div class="relative">
-                                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                                            </div>
-                                            <input datepicker datepicker-autohide type="text" class="edit-agenda-deadline bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date" name="deadline" value="">
-                                        </div> --}}
-                                        
                                         <label for="date" class="block text-sm font-medium text-gray-700">Date</label>
                                         <div class="mt-1 relative rounded-md shadow-sm">
                                             <input type="date" name="deadline" id="date" class="edit-agenda-deadline focus:ring-indigo-500 focus:border-indigo-500 block w-full pr-10 sm:text-sm border-gray-300 rounded-md">
                                         </div>
-                
+
                                         @error('deadline')
                                             <p id="filled_error_help" class="mt-2 text-xs text-red-600 dark:text-red-400"><span class="font-medium">{{ $message }}</span></p>
                                         @enderror
